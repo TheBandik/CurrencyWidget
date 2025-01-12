99c3cdf33a2e5ebcb2b80d59ec658c60569be331
Arkadiy Schneider <shnr.arky@gmail.com>
2025-01-12 19:34:54 +1000

    feat: init project

---
+++ CurrencyWidget/CurrencyWidget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CurrencyWidget
+//
+//  Created by Arkadiy Schneider on 12.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CurrencyWidget/CurrencyWidget/CurrencyWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyWidgetApp.swift
+//  CurrencyWidget
+//
+//  Created by Arkadiy Schneider on 12.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
